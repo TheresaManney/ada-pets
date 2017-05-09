719b595422eac6bc398c6a96195e346a4b6537da
Theresa Manney <theresa.m.17@gmail.com>
2017-05-09 09:33:13 -0700

    got rid of the created at info

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,7 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
-    render json: @pets
+    # No need for instance variable since we will not be calling this info in a view
+    pets = Pet.all
+    render json: pets.as_json(only: [:id, :name, :age, :human])
   end
 end
