28f67be90cc2e4a505065607b89e95fa532f4e60
Theresa Manney <theresa.m.17@gmail.com>
2017-05-09 10:38:41 -0700

    added create path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get "pets", to: "pets#index", as: "pets"
-  get "pets/:id", to: "pets#show", as: "pet"
+  get "/pets", to: "pets#index", as: "pets"
+  get "/pets/:id", to: "pets#show", as: "pet"
+  post "/pets", to: "pets#create"
 
 end
